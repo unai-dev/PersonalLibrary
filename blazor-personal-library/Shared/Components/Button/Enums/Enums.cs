d541bea02aa9c8973811b162d669ffa384414907
Unai <unaidevcoding@gmail.com>
2026-05-09 20:02:53 +0200

    add validations at add new book & add personalize cssclases in bpbutton

---
+++ blazor-personal-library/Shared/Components/Button/Enums/Enums.cs
@@ -0,0 +1,19 @@
+﻿namespace blazor_personal_library.Shared.Components.Button.Enums
+{
+    public enum Variant
+    {
+        Primary,
+        Secondary,
+        Success,
+        Warning,
+        Error
+
+    }
+
+    public enum Width
+    {
+        Full,
+        Half,
+        Small
+    }
+}
